e273790393bc809218dbe665c6904ee1d43cb3fe
Chetan Kini <chetan@superconductive.com>
2022-04-23 12:40:56 -0400

    feat: allow pip installable through setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from typing import List
+
+import setuptools
+
+
+def get_requirements() -> List[str]:
+    with open("requirements.txt") as f:
+        requirements = f.read().splitlines()
+    return requirements
+
+
+setuptools.setup(
+    name="ge_releaser",
+    version="0.1.0",
+    install_requires=get_requirements(),
+    entry_points="""
+      [console_scripts]
+      ge_releaser=ge_releaser.cli:cli
+    """,
+)
