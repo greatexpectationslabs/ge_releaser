4b37bf34e56e562f3b42ff89c8e0d471d80cafce
Chetan Kini <chetan@superconductive.com>
2022-05-26 08:51:44 -0400

    feat: don't assume that release commit is HEAD

--- ge_releaser/tag.py
+++ ge_releaser/tag.py
@@ -1,7 +1,9 @@
 import os
+from typing import List, Optional
 
 import click
 import git
+from git.objects.commit import Commit
 
 from ge_releaser.cli import GitEnvironment
 from ge_releaser.constants import AZURE_BUILDS, PYPI_PAGE, RELEASES
@@ -14,7 +16,7 @@ def tag(env: GitEnvironment) -> None:
     checkout_and_update_develop(env.git_repo)
     release_version: str = str(parse_deployment_version_file())
 
-    _tag_latest_develop(env.git_repo, release_version)
+    _tag_release_commit(env.git_repo, release_version)
     click.secho(" * Tagged latest commit on develop (1/2)", fg="yellow")
 
     _push_tag_to_remote(env.git_repo, release_version)
@@ -35,7 +37,19 @@ def tag(env: GitEnvironment) -> None:
     click.echo(f"Link to PyPI page: {PYPI_PAGE}")
 
 
-def _tag_latest_develop(git_repo: git.Repo, release_version: str) -> None:
+def _tag_release_commit(git_repo: git.Repo, release_version: str) -> None:
+    commits: List[Commit] = list(git_repo.iter_commits("develop", max_count=5))
+
+    commit_to_tag: Optional[Commit] = None
+    for commit in commits:
+        if "RELEASE" in str(commit.message):
+            commit_to_tag = commit
+            break
+
+    if commit_to_tag is None:
+        raise ValueError("Could not find release commit!")
+
+    git_repo.git.checkout(commit_to_tag.hexsha)
     git_repo.git.tag("-a", release_version, "-m", f'"{release_version}"')
 
 
