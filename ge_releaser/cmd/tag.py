348ab748718c105b4b74da4ed1ef7208b914ba7e
Chetan Kini <chetan@superconductive.com>
2023-11-14 15:26:43 -0500

    add flag

--- ge_releaser/cmd/tag.py
+++ ge_releaser/cmd/tag.py
@@ -1,12 +1,14 @@
 import os
 
 import click
+from packaging import version
 
 from ge_releaser.constants import GxURL
 from ge_releaser.git import GitService
 
 
-def tag(git: GitService, commit: str, version_number: str) -> None:
+def tag(git: GitService, commit: str, version_number: str, is_pre_release: bool) -> None:
+    _check_version_validity(version_number=version_number, is_pre_release=is_pre_release)
     click.secho("[tag]", bold=True, fg="blue")
 
     _tag_release_commit(git, commit, version_number)
@@ -15,7 +17,17 @@ def tag(git: GitService, commit: str, version_number: str) -> None:
     git.push_branch_to_remote(branch=version_number, set_upstream=False)
     click.secho(" * Pushed tag to remote (2/2)", fg="yellow")
 
-    _print_next_steps(version_number)
+    _print_next_steps(version_number=version_number, is_pre_release=is_pre_release)
+
+
+def _check_version_validity(version_number: str, is_pre_release: bool) -> None:
+    v = version.parse(version_number)
+
+    if is_pre_release and not v.is_prerelease:
+        raise ValueError("Passed in --pre-release flag but did not provide a version with a pre-release suffix ('a', 'b', 'rc')")
+
+    if not is_pre_release and v.is_prerelease:
+        raise ValueError("Passed in a pre-release version but did not turn on the --pre-release flag")
 
 
 def _tag_release_commit(git: GitService, commit: str, release_version: str) -> None:
@@ -27,12 +39,15 @@ def _tag_release_commit(git: GitService, commit: str, release_version: str) -> N
     git.tag_commit(commit=commit, version=release_version)
 
 
-def _print_next_steps(version_number: str) -> None:
+def _print_next_steps(version_number: str, is_pre_release: bool) -> None:
     tag_url = os.path.join(GxURL.RELEASES, "tag", version_number)
-    click.secho(
-        "\nPlease wait for the build process and PyPI publishing to complete before moving to the `prep` cmd.",
-        fg="green",
-    )
+
+    if is_pre_release:
+        msg = "No additional steps are required when publishing a pre-release."
+    else:
+        msg = "Please wait for the build process and PyPI publishing to complete before moving to the `prep` cmd."
+
+    click.secho(f"\n{msg}", fg="green")
     click.echo(f"Link to tag: {tag_url}")
     click.echo(f"Link to Azure build: {GxURL.AZURE_BUILDS}")
     click.echo(f"Link to PyPI page: {GxURL.PYPI_PAGE}")
