f8b2e067dd17f7e5af4d482840b8f09ff016a529
Gabriel Gore <gabriel59kg@gmail.com>
2023-12-08 12:59:26 -0500

    add 'GE_RELEASE_TRUNK' env var override

--- ge_releaser/utils.py
+++ ge_releaser/utils.py
@@ -10,6 +10,7 @@ from ge_releaser.constants import (
     RELEASER_LOCAL_VERSION,
     RELEASER_REMOTE_VERSION,
     REMOTE,
+    PRE_V1_TRUNK,
     TRUNK,
     GxFile,
 )
@@ -49,13 +50,20 @@ def _get_latest_version() -> str:
 
 def setup(ctx: click.Context) -> None:
     token: Optional[str] = os.environ.get("GITHUB_TOKEN")
+
     assert token is not None, "Must set GITHUB_TOKEN environment variable!"
+
+    trunk_override: Optional[str] = os.environ.get("GX_RELEASE_TRUNK")
+    if trunk_override and trunk_override != TRUNK:
+        input(
+            f"WARNING: GX_RELEASE_TRUNK is set to {trunk_override}. Press enter to continue. CTRL+C to exit."
+        )
 
     check_if_in_gx_root()
     check_if_using_latest_version()
 
     git = GitService(
-        github_token=token, repo_name=GITHUB_REPO, trunk=TRUNK, remote=REMOTE
+        github_token=token, repo_name=GITHUB_REPO, trunk=trunk_override or TRUNK, remote=REMOTE
     )
     git.verify_no_untracked_files()
     ctx.obj = git
